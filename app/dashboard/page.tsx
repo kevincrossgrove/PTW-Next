e6b2b0b66cdc0048a9bffe815862ed0343ab37ab
kevincrossgrove <crossgrovekevin@gmail.com>
2025-06-04 15:14:39 -0400

    Improve dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,26 @@
 "use client";
 
+import AppNavbar from "@/components/app/AppNavbar";
 import { Button } from "../../components/ui/button";
 import { authClient } from "../../lib/auth-client";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function DashboardPage() {
   const router = useRouter();
 
   return (
-    <div>
-      DashboardPage<Button onClick={signout}>Signout</Button>
+    <div className="flex flex-col h-screen">
+      <AppNavbar />
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-4xl font-bold mb-8">Dashboard</h1>
+        <div className="flex gap-2">
+          <Button variant="outline" asChild>
+            <Link href="/admin/home">Admin Dashboard</Link>
+          </Button>
+          <Button onClick={signout}>Sign Out</Button>
+        </div>
+      </div>
     </div>
   );
 
